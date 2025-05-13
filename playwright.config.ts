e55b0e87063c87d9200ef88db3f47d551b8a031e
Tevan <tevanp@gmail.com>
2025-05-14 00:36:34 +0200

    update env file

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, devices } from '@playwright/test';
 
 /**
  * Read environment variables from file.
- * https://github.com/motdotla/dotenv
+ * https://github.com/motdotla/doten
  */
 import dotenv from 'dotenv';
 import path from 'path';
